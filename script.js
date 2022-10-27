2a1fc79ae7c8f6834a0e377f209e8c8d031325f1
KurtReti <68139188+KurtReti@users.noreply.github.com>
2022-10-27 22:02:00 +1100

    color shift

--- script.js
+++ script.js
@@ -1,4 +1,76 @@
-var countDownDate = new Date("Oct 27, 2022, 21:17:55").getTime();
+function BCD(){
+    clearInterval(x);
+    document.querySelector(".countdown").style = "display: none"
+
+    document.querySelector(".bcd-wrapper").style ="display: flex"
+
+    setTimeout(function(){
+        document.querySelector(".big-wrapper").style = "opacity: 1"
+
+    }, 1000)
+
+    setTimeout(function(){
+        document.querySelector(".cocks-wrapper").style = "opacity: 1"
+
+    }, 2000)
+
+    setTimeout(function(){
+        document.querySelector(".divided-wrapper").style = "opacity: 1"
+
+    }, 3000)
+
+
+    setTimeout(function(){
+        document.querySelector(".divided-wrapper").style = "opacity: 1"
+
+    }, 4000)
+
+    setTimeout(function(){
+        document.querySelector(".divided-wrapper").style = "opacity: 0"
+
+    }, 5000)
+
+    setTimeout(function(){
+        document.querySelector(".divided-wrapper").style = "opacity: 1"
+
+    }, 6000)
+
+    setTimeout(function(){
+        document.querySelector(".divided-wrapper").style = "opacity: 0"
+
+    }, 7000)
+
+    setTimeout(function(){
+        document.querySelector(".divided-wrapper").style = "opacity: 1"
+
+    }, 8000)
+
+
+    setTimeout(function(){
+        document.querySelector(".divided-wrapper").style = "opacity: 0"
+
+    }, 9000)
+
+
+     setTimeout(function(){
+        document.querySelector(".divided-wrapper").innerHTML = "DIVIDED"
+        document.querySelector(".divided-wrapper").style = "opacity: 1"
+
+    }, 11000)
+
+    setTimeout(function(){
+        document.querySelector(".divided-wrapper").style.color = "red"
+        document.querySelector(".big-wrapper").style.color = "red"
+        document.querySelector(".cocks-wrapper").style.color = "red"
+
+    }, 13000)
+
+
+
+
+}
+
+var countDownDate = new Date("Oct 27, 2022, 21:54:00").getTime();
 
 var x = setInterval(async function(){
 
@@ -15,96 +87,9 @@ var x = setInterval(async function(){
     document.querySelector(".countdown").innerHTML = (String(days).padStart(2, '0') + ':' + String(hours).padStart(2, '0') +':' + String(minutes).padStart(2, '0') + ":" + String(seconds).padStart(2, '0'));
 
     if (distance < 0) {
-        clearInterval(x);
-        document.querySelector(".countdown").style = "display: none"
-
-        setTimeout(function(){
-            document.querySelector(".big-wrapper").style = "opacity: 1"
-
-        }, 1000)
-
-        setTimeout(function(){
-            document.querySelector(".cocks-wrapper").style = "opacity: 1"
-
-        }, 2000)
-
-        setTimeout(function(){
-            document.querySelector(".divided-wrapper").style = "opacity: 1"
-
-        }, 3000)
-
-        setTimeout(function(){
-            document.querySelector(".divided-wrapper").style = "opacity: 0"
-
-        }, 3500)
-
-        setTimeout(function(){
-            document.querySelector(".divided-wrapper").style = "opacity: 1"
-
-        }, 4000)
-
-        setTimeout(function(){
-            document.querySelector(".divided-wrapper").style = "opacity: 0"
-
-        }, 4500)
-
-        setTimeout(function(){
-            document.querySelector(".divided-wrapper").style = "opacity: 1"
-
-        }, 5000)
-
-        setTimeout(function(){
-            document.querySelector(".divided-wrapper").style = "opacity: 0"
-
-        }, 5500)
-
-        setTimeout(function(){
-            document.querySelector(".divided-wrapper").style = "opacity: 1"
-
-        }, 5750)
-
-        setTimeout(function(){
-            document.querySelector(".divided-wrapper").style = "opacity: 0"
-
-        }, 6000)
-
-
-        setTimeout(function(){
-            document.querySelector(".divided-wrapper").style = "opacity: 1"
-
-        }, 6250)
-
-    
-    
-        setTimeout(function(){
-            document.querySelector(".divided-wrapper").style = "opacity: 1"
-
-        }, 6250)
         
-        setTimeout(function(){
-            document.querySelector(".divided-wrapper").style = "opacity: 0"
-
-        }, 6500)
-        
-        setTimeout(function(){
-            document.querySelector(".divided-wrapper").style = "opacity: 1"
-
-        }, 6750)
-        
-        setTimeout(function(){
-            document.querySelector(".divided-wrapper").style = "opacity: 0"
-
-        }, 7000)
-
-
-         setTimeout(function(){
-            document.querySelector(".divided-wrapper").innerHTML = "DIVIDED"
-            document.querySelector(".divided-wrapper").style = "opacity: 1"
-
-        }, 9000)
-
       
-
+        BCD();
       
       
      
